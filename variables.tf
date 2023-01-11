7dd91241e62b4ab8dceb00f69cdfcfc0e858e54e
Troy Fluegge <troy@hashicorp.com>
2023-01-11 16:08:11 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,24 @@
-variable "db_name" {
-  description = "Unique name to assign to RDS instance"
+variable "vpc_name" {
+  description = "Unique name to assign to VPC"
 }
 
-variable "db_username" {
-  description = "RDS root username"
+variable "sg_name" {
+  description = "Security Group Name"
 }
 
-variable "db_password" {
-  description = "RDS root user password"
-  sensitive   = true
+variable "region" {
+  description = "AWS Region"
 }
+
+# variable "db_name" {
+#   description = "Unique name to assign to RDS instance"
+# }
+
+# variable "db_username" {
+#   description = "RDS root username"
+# }
+
+# variable "db_password" {
+#   description = "RDS root user password"
+#   sensitive   = true
+# }

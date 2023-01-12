7cb9a44f13bd8c6f93333824561d74cb253f0b14
Troy Fluegge <troy@hashicorp.com>
2023-01-11 22:11:53 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,18 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "sg_id" {
+  description = "Security Group ID"
+  value       = aws_security_group.sg.id
+}
+
+output "random_pet" {
+  description = "Random Pet Name"
+  value       = random_pet.random.id
+}
+ 
 # output "rds_hostname" {
 #   description = "RDS instance hostname"
 #   value       = aws_db_instance.education.address
